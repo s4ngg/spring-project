d092e4cf6251725c6d365c2a63b1550e9767a082
s4ngg <s4ngg@naver.com>
2026-02-23 14:36:36 +0900

    Fix(header) : 종 모양 알람 수정 Fix(home) : 캘린더 + 춡퇴근 버튼 프론트부부 ㄴ 수정

--- src/main/java/kr/co/spring_project/attendance/repository/AttendanceRepository.java
+++ src/main/java/kr/co/spring_project/attendance/repository/AttendanceRepository.java
@@ -12,4 +12,6 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long>{
 		    Long employeeNo, LocalDateTime start, LocalDateTime end);
 
 	List<Attendance> findByCheckInBetween(LocalDateTime start, LocalDateTime end);
+	List<Attendance> findByMember_EmployeeNoAndCheckOutBetween(
+		    Long employeeNo, LocalDateTime start, LocalDateTime end);
 }
